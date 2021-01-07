4cdf5495f68e3036f7f6a715290d12d28967e568
Armin Gholampoor <armingh379@gmail.com>
2021-01-07 00:13:51 -0500

    add rename_field

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ import argparse
 
 from antlr4 import *
 
-from refactorings.extract_class import ExtractClassRefactoringListener
-from gen.java9 import Java9_v2Lexer
+#from refactorings.extract_class import ExtractClassRefactoringListener
+from refactorings.rename_field import RenameFieldRefactoringListener
+from gen.java9.Java9_v2Lexer import Java9_v2Lexer
 from gen.java9.Java9_v2Parser import Java9_v2Parser
 
-from java9speedy.parser import sa_java9_v2
-
+#from speedy.src.java9speedy.parser import sa_java9_v2
 
 def main(args):
     # Step 1: Load input source into stream
@@ -33,7 +33,7 @@ def main(args):
     token_stream = CommonTokenStream(lexer)
     # Step 4: Create an instance of the AssignmentStParser
     parser = Java9_v2Parser(token_stream)
-    parser.getTokenStream()
+    #parser.getTokenStream()
 
     # Step 5: Create parse tree
     # 1. Python backend --> Low speed
@@ -41,14 +41,16 @@ def main(args):
 
     # 2. C++ backend --> high speed
 
-    parse_tree = sa_java9_v2.parse(stream, 'compilationUnit', None)
-    quit()
+    #parse_tree = sa_java9_v2.parse(stream, 'compilationUnit', None)
+    #quit()
     # Step 6: Create an instance of AssignmentStListener
-    my_listener = ExtractClassRefactoringListener(common_token_stream=token_stream, class_identifier='Worker')
+    #my_listener = ExtractClassRefactoringListener(common_token_stream=token_stream, class_identifier='Worker')
+    tree = parser.compilationUnit()
+    my_listener = RenameFieldRefactoringListener(common_token_stream=token_stream, class_identifier="A", field_identifier="g", new_field_identifier="gg")
 
     # return
     walker = ParseTreeWalker()
-    walker.walk(t=parse_tree, listener=my_listener)
+    walker.walk(t=tree, listener=my_listener)
 
     with open('input.refactored.java', mode='w', newline='') as f:
         f.write(my_listener.token_stream_rewriter.getDefaultText())
@@ -58,6 +60,6 @@ if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument(
         '-n', '--file',
-        help='Input source', default=r'../grammars/Test.java')
+        help='Input source', default=r'input.java')
     args = argparser.parse_args()
     main(args)
---
+++ refactorings/rename_field.py
@@ -0,0 +1,109 @@
+from antlr4 import *
+from gen.java9.Java9_v2Listener import Java9_v2Listener
+from gen.java9.Java9_v2Parser import Java9_v2Parser
+from antlr4.TokenStreamRewriter import TokenStreamRewriter
+
+class RenameFieldRefactoringListener (Java9_v2Listener):
+
+    def __init__ (self, field_identifier : str , new_field_identifier : str, class_identifier : str = None, common_token_stream: CommonTokenStream = None):
+        self.enter_class = False if class_identifier else True
+        self.new_field_identifier = new_field_identifier
+        self.class_identifier = class_identifier
+        self.field_identifier = field_identifier
+
+        if common_token_stream is not None:
+            self.token_stream_rewriter = TokenStreamRewriter(common_token_stream)
+        else:
+            raise TypeError('common_token_stream is None')
+
+
+    # Enter a parse tree produced by Java9_v2Parser#normalClassDeclaration.
+    def enterNormalClassDeclaration(self, ctx:Java9_v2Parser.NormalClassDeclarationContext):
+        if ctx.identifier().getText() != self.class_identifier:
+            return
+        self.enter_class = True
+
+    # Exit a parse tree produced by Java9_v2Parser#normalClassDeclaration.
+    def exitNormalClassDeclaration(self, ctx:Java9_v2Parser.NormalClassDeclarationContext):
+        if self.enter_class and self.class_identifier:
+            self.enter_class = False
+
+    # Enter a parse tree produced by Java9_v2Parser#variableDeclaratorId.
+    def enterVariableDeclaratorId(self, ctx:Java9_v2Parser.VariableDeclaratorIdContext):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#variableDeclaratorId.
+    def exitVariableDeclaratorId(self, ctx:Java9_v2Parser.VariableDeclaratorIdContext):
+        pass
+
+    # Enter a parse tree produced by Java9_v2Parser#fieldAccess1.
+    def enterFieldAccess1(self, ctx:Java9_v2Parser.FieldAccess1Context):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess1.
+    def exitFieldAccess1(self, ctx:Java9_v2Parser.FieldAccess1Context):
+        pass
+
+
+    # Enter a parse tree produced by Java9_v2Parser#fieldAccess2.
+    def enterFieldAccess2(self, ctx:Java9_v2Parser.FieldAccess2Context):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess2.
+    def exitFieldAccess2(self, ctx:Java9_v2Parser.FieldAccess2Context):
+        pass
+
+
+    # Enter a parse tree produced by Java9_v2Parser#fieldAccess3.
+    def enterFieldAccess3(self, ctx:Java9_v2Parser.FieldAccess3Context):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess3.
+    def exitFieldAccess3(self, ctx:Java9_v2Parser.FieldAccess3Context):
+        pass
+
+        # Enter a parse tree produced by Java9_v2Parser#fieldAccess_lf_primary.
+    def enterFieldAccess_lf_primary(self, ctx:Java9_v2Parser.FieldAccess_lf_primaryContext):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess_lf_primary.
+    def exitFieldAccess_lf_primary(self, ctx:Java9_v2Parser.FieldAccess_lf_primaryContext):
+        pass
+
+    # Enter a parse tree produced by Java9_v2Parser#fieldAccess_lfno_primary1.
+    def enterFieldAccess_lfno_primary1(self, ctx:Java9_v2Parser.FieldAccess_lfno_primary1Context):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess_lfno_primary1.
+    def exitFieldAccess_lfno_primary1(self, ctx:Java9_v2Parser.FieldAccess_lfno_primary1Context):
+        pass
+
+
+    # Enter a parse tree produced by Java9_v2Parser#fieldAccess_lfno_primary2.
+    def enterFieldAccess_lfno_primary2(self, ctx:Java9_v2Parser.FieldAccess_lfno_primary2Context):
+        if self.enter_class:
+            if ctx.identifier().getText() == self.field_identifier:
+                interval = ctx.identifier().getSourceInterval()
+                self.token_stream_rewriter.replaceRange(interval[0], interval[1], text = self.new_field_identifier)
+
+    # Exit a parse tree produced by Java9_v2Parser#fieldAccess_lfno_primary2.
+    def exitFieldAccess_lfno_primary2(self, ctx:Java9_v2Parser.FieldAccess_lfno_primary2Context):
+        pass
